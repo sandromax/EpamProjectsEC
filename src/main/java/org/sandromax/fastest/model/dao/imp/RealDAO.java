4806d7b589ebb2b6aed0d5016569125ff7362b67
sandromax <sandrondk@gmail.com>
2018-01-24 12:10:53 +0200

    added selectDate... and selectSuccess... in DAO

--- src/main/java/org/sandromax/fastest/model/dao/imp/RealDAO.java
+++ src/main/java/org/sandromax/fastest/model/dao/imp/RealDAO.java
@@ -12,7 +12,9 @@ public class RealDAO {
     public static final String SQL_INSERT_STUDENT = "INSERT INTO students(name, surname, email, pass) VALUES(?, ?, ?, ?)";
     public static final String SQL_SELECT_ALL_STUDENTS = "SELECT * FROM students";
     public static final String SQL_SELECT_STUDENTS_EMAIL_BY_PASS = "SELECT email FROM students WHERE pass = '?'";
-    public static final String SQL_SELECT_RATE_BY_STUDENT_AND_NAME = "SELECT rate FROM success WHERE student_id = ? AND theme_id = ?";
+    public static final String SQL_SELECT_RATE_BY_STUDENT_AND_THEME = "SELECT rate FROM success WHERE student_id = ? AND theme_id = ?";
+    public static final String SQL_SELECT_DATE_BY_STUDENT_AND_THEME = "SELECT date FROM success WHERE student_id = ? AND theme_id = ?";
+    public static final String SQL_SELECT_SUCCESS_BY_STUDENT = "SELECT * FROM success WHERE student_id = ?";
 
 
     boolean addStudent(String name, String surname, String email, String pass) {
@@ -95,19 +97,73 @@ public class RealDAO {
     }
 
     Double getRateByStudentAndTheme(int studentId, int themeId) {
-        return 0.0;
+        PreparedStatement preparedStatement = null;
+        Double rate = null;
+
+        try (Connection connection = ConnectionPool.getConnection()) {
+            connection.prepareStatement(SQL_SELECT_RATE_BY_STUDENT_AND_THEME);
+
+            preparedStatement.setInt(1, studentId);
+            preparedStatement.setInt(2, themeId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            rate = resultSet.getDouble("rate");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return rate;
     }
 
     LocalDate getDateByStudentAndTheme(int studentId, int themeId) {
-        return LocalDate.now();
+        PreparedStatement preparedStatement = null;
+        LocalDate date = null;
+        Date dateSQL = null;
+
+        try (Connection connection = ConnectionPool.getConnection()) {
+            connection.prepareStatement(SQL_SELECT_DATE_BY_STUDENT_AND_THEME);
+
+            preparedStatement.setInt(1, studentId);
+            preparedStatement.setInt(2, themeId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            dateSQL = resultSet.getDate("date");
+            date = dateSQL.toLocalDate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return date;
     }
 
     TestHistory getHistoryByStudentAndTheme(int studentId, int themeId) {
         return new TestHistory();
     }
 
-    Success getSuccessByStudent(int studentId) {
-        return new Success();
+    List<Success> getSuccessByStudent(int studentId) {
+        PreparedStatement preparedStatement = null;
+        LocalDate date = null;
+        Date dateSQL = null;
+        Success success = new Success();
+        List <Success> successes = new LinkedList<>();
+
+        try (Connection connection = ConnectionPool.getConnection()) {
+            connection.prepareStatement(SQL_SELECT_SUCCESS_BY_STUDENT);
+
+            preparedStatement.setInt(1, studentId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return successes;
     }
 
     List<Subject> getAllSubjects() {
@@ -123,6 +179,10 @@ public class RealDAO {
     }
 
     boolean setSuccessOfStudent(Student student, Success success) {
+        return true;
+    }
+
+    boolean addIssues(LinkedList<Issue> issues) {
         return true;
     }
 }
