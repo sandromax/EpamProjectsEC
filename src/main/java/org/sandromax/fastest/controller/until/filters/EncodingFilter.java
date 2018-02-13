624310e92c2080b49afe8f1229831cc105b45cf4
sandromax <sandrondk@gmail.com>
2018-02-13 22:45:20 +0200

    Adding Encoding Filter

---
+++ src/main/java/org/sandromax/fastest/controller/until/filters/EncodingFilter.java
@@ -0,0 +1,34 @@
+package org.sandromax.fastest.controller.until.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import java.io.IOException;
+@WebFilter(urlPatterns = {"/*"},
+            initParams = {
+            @WebInitParam(name = "encoding", value = "UTF-8", description = "Encoding Param") })
+public class EncodingFilter implements Filter {
+    private String code;
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        code = filterConfig.getInitParameter("encoding");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String codeRequest = servletRequest.getCharacterEncoding();
+
+        if(code != null && !code.equalsIgnoreCase(codeRequest)) {
+            servletRequest.setCharacterEncoding(code);
+            servletResponse.setCharacterEncoding(code);
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("doFilter: " + servletRequest.getCharacterEncoding());
+    }
+
+    @Override
+    public void destroy() {
+        code = null;
+    }
+}
