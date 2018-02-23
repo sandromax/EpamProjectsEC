1c374967c1522c1d1c190ec827dad3276a27b42d
sandromax <sandrondk@gmail.com>
2018-02-23 22:16:04 +0200

    add some constants

--- src/main/java/org/sandromax/fastest/controller/until/constants/Pages.java
+++ src/main/java/org/sandromax/fastest/controller/until/constants/Pages.java
@@ -13,15 +13,17 @@ package org.sandromax.fastest.controller.until.constants;
  }
  */
 public interface Pages {
-    String START_PAGE = "index.jsp";
-    String REGISTRATION_PAGE = "jsp/sign_in.jsp";
+    String INDEX_PAGE = "index.jsp";
+    String SIGN_IN_PAGE = "jsp/sign_in.jsp";
     String SIGN_UP_PAGE = "jsp/sign_up.jsp";
 
     String SUBJECTS_CATALOG = "jsp/subjects_catalog.jsp";
-    String THEMES_CATALOG = "jsp/catalog_themes.jsp";
+    String THEMES_CATALOG = "jsp/themes_catalog.jsp";
 
-    String TEST = "jsp/test.jsp";
+    String TEST_PAGE = "jsp/test_page.jsp";
     String TEST_RESULT = "jsp/test_result.jsp";
 
     String ADMIN_PAGE = "jsp/admin_page.jsp";
+
+    String TEST_FIELD_PAGE = "jsp/test_field_page.jsp";
 }
---
+++ src/main/java/org/sandromax/fastest/controller/until/constants/Parameter.java
@@ -0,0 +1,11 @@
+package org.sandromax.fastest.controller.until.constants;
+
+public interface Parameter {
+    String USER_NAME = "user_name";
+    String EMAIL = "email";
+    String PASSWORD = "password";
+
+    String SUBJECT = "subject";
+
+    String COMMAND = "command";
+}
