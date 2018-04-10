ea6741104c62ed17dc30747d93c470756fc7c965
sandromax <sandrondk@gmail.com>
2018-04-10 12:10:04 +0300

    Created class TestResult. (Method style by R. Martin)

---
+++ src/main/java/org/sandromax/fastest/domain/testing/TestResult.java
@@ -0,0 +1,95 @@
+package org.sandromax.fastest.domain.testing;
+
+import org.sandromax.fastest.domain.test.Theme;
+import org.sandromax.fastest.domain.user.Student;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class TestResult {
+    private Theme theme;
+    private Student student;
+    private LocalDateTime timeMoment;
+
+    private LinkedList<IssueDone> issueDones = new LinkedList<>();
+
+    private boolean isRated;
+    private double rate;
+
+    public TestResult() { }
+
+    public TestResult(Theme theme, Student student) {
+        this.theme = theme;
+        this.student = student;
+    }
+
+    public void addIssueDone(IssueDone issueDone) {
+        issueDones.add(issueDone);
+    }
+
+    public void setTimeMoment(LocalDateTime timeMoment) {
+        this.timeMoment = timeMoment;
+    }
+
+    public void evaluateIssues() {
+        int positiveAnswers = 0;
+
+        if(!isRated) {
+            for(IssueDone issueDone : issueDones) {
+                if(issueDone.getPositive())
+                    positiveAnswers++;
+            }
+        }
+
+        rate = positiveAnswers;
+    }
+
+    public LinkedList<IssueDone> getIssueDones() {
+        return issueDones;
+    }
+
+    public String getIssueDonesDbFormat() {
+        List<String> idList = new LinkedList<>();
+
+        for(IssueDone issueDone : issueDones) {
+            if(issueDone.getIdInDb() != 0)
+                idList.add("" + issueDone.getIdInDb());
+            else
+                return "";
+        }
+
+        return joinForDb(idList);
+    }
+
+    public LocalDateTime getTimeMoment() {
+        return timeMoment;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+
+    private static String joinForDb(List<String> list) {
+
+        StringJoiner joiner = new StringJoiner("^");
+
+        for(String str : list) {
+            joiner.add(str.toString());
+        }
+
+        String result = joiner.toString();
+
+        return result;
+    }
+}
